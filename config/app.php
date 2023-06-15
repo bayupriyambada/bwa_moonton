9349f06a738f6735b2661c1858cc6588ac4028a6
yamayuba00 <yamayuba00@gmail.com>
2023-06-15 11:09:34 +0700

    Spatie Permission

--- config/app.php
+++ config/app.php
@@ -166,8 +166,8 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        App\Providers\EventServiceProvider::class, App\Providers\RouteServiceProvider::class, Spatie\Permission\PermissionServiceProvider::class,
+
     ])->toArray(),
 
     /*
